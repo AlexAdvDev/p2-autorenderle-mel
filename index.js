e25240321b57738395054fb9a18dff2ba402d701
Jaio <jaio0701@gmail.com>
2023-04-03 18:48:06 -0400

    implement basic game (only correct/incorrect)

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const apiBase = "https://autorender.portal2.sr/api/v1";
 const boardsBase = "https://board.portal2.sr";
 
 // Get a random map ID from maplist.js
-const randomMap = Math.floor(Math.random() * 60) + 1;
+var randomMap = Math.floor(Math.random() * 60) + 1;
 var mapID = mapsJson[randomMap - 1].mapid;
 console.log("Map ID: " + mapID + " (map number " + randomMap + ")");
 
 var boardJson = null;
 var runID = null;
+var trueRank = null;
 $(document).ready(function() {
     // Get the boards JSON
     $.getJSON(boardsBase + "/chamber/" + mapID + "/json", function(data) {
@@ -16,8 +17,8 @@ $(document).ready(function() {
     }).then(function() {
         function generateRunID() {
             // Generate a random rank between 1 and 200 (slightly broken)
-            const randomRank = Math.floor(Math.random() * 200) + 1;
-            const trueRank = randomRank +1 ;
+            var randomRank = Math.floor(Math.random() * 200) + 1;
+            trueRank = randomRank +1 ;
             console.log("Rank: " + trueRank);
 
             // Get the run ID from boards api
@@ -39,3 +40,22 @@ $(document).ready(function() {
         videoPlayer.src = apiBase + "/video/" + runID + "/video";
     });
 });
+
+// Submitted Guess function
+var submitText = document.getElementById("rankGuess");
+submitText.onkeyup = function(e) {
+    if(e.keyCode == 13){
+       submitGuess();
+    }
+}
+function submitGuess() {
+    var guess = submitText.value;
+    console.log("Guess: " + guess);
+
+    // Check if guess is correct
+    if(guess == trueRank) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect!");
+    }
+}
