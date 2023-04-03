070b8d66981b4e061468ab308f21d3d0b716f77f
Jaio <jaio0701@gmail.com>
2023-04-03 16:22:28 -0400

    fix random autorender displaying
    There is a bug where runs without demos (thus no autorender) do crash

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const apiBase = "https://autorender.portal2.sr/api/v1";
 const boardsBase = "https://board.portal2.sr";
 
+// Get a random map ID from maplist.js
 const randomMap = Math.floor(Math.random() * 60) + 1;
 var mapID = mapsJson[randomMap - 1].mapid;
 console.log("Map ID: " + mapID + " (map number " + randomMap + ")");
 
-// currently not working, getting this error:
-// Uncaught TypeError: Cannot read properties of undefined (reading 'keys')
-// from line 24, specifically "keys[randomRank - 1]"
-var boardJson;
-var keys;
+var boardJson = null;
+var runID = null;
+$(document).ready(function() {
+    // Get the boards JSON
+    $.getJSON(boardsBase + "/chamber/" + mapID + "/json", function(data) {
+        boardJson = data;
+        console.log(boardJson);
+    }).then(function() {
+        // Generate a random rank between 1 and 200 (slightly broken)
+        const randomRank = Math.floor(Math.random() * 200) + 1;
+        const trueRank = randomRank +1 ;
+        console.log("Rank: " + trueRank);
 
-fetch('https://board.portal2.sr/chamber/' + mapID + '/json').then(function(response) {
-    return response.json();
-}).then(function(obj) {
-    console.log(obj);
-    boardJson = obj;
-    keys = Object.keys(boardJson);
-    console.log(keys);
+        // Get the run ID from the random rank and boards
+        runID = boardJson[Object.keys(boardJson)[randomRank]].scoreData.changelogId;
+        console.log("Run ID: " + runID);
+
+        // Display autorender
+        var videoPlayer = document.getElementById("videoPlayer");
+        videoPlayer.src = apiBase + "/video/" + runID + "/video";
+    });
 });
-
-const randomRank = Math.floor(Math.random() * 200) + 1;
-console.log("Rank: " + randomRank);
-var runID = boardJson.keys[randomRank - 1].scoreData.changelogId;
-//console.log("Run ID: " + runID);
-
-var videoPlayer = document.getElementById("videoPlayer");
-//videoPlayer.src = apiBase + "/video/" + runID + "/video";
