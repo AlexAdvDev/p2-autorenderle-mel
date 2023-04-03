699792cf53db03dc49dd1d4c6dfe7ec5045f76e0
Jaio <jaio0701@gmail.com>
2023-04-03 16:43:19 -0400

    hasDemo == 0 fix
    There are some runs that have demos but no autorender, I'll deal with those later lol

--- index.js
+++ index.js
@@ -14,14 +14,25 @@ $(document).ready(function() {
         boardJson = data;
         console.log(boardJson);
     }).then(function() {
-        // Generate a random rank between 1 and 200 (slightly broken)
-        const randomRank = Math.floor(Math.random() * 200) + 1;
-        const trueRank = randomRank +1 ;
-        console.log("Rank: " + trueRank);
+        function generateRunID() {
+            // Generate a random rank between 1 and 200 (slightly broken)
+            const randomRank = Math.floor(Math.random() * 200) + 1;
+            const trueRank = randomRank +1 ;
+            console.log("Rank: " + trueRank);
 
-        // Get the run ID from the random rank and boards
-        runID = boardJson[Object.keys(boardJson)[randomRank]].scoreData.changelogId;
-        console.log("Run ID: " + runID);
+            // Get the run ID from boards api
+            runID = boardJson[Object.keys(boardJson)[randomRank]].scoreData.changelogId;
+            console.log("Run ID: " + runID);
+
+            // Check if the run ID has a valid demo (and autorender)
+            if(boardJson[Object.keys(boardJson)[randomRank]].scoreData.hasDemo == 0) {
+                console.error("Run ID has no autorender, generating new run ID");
+                generateRunID();
+            }
+        }
+
+        // Generate the run ID
+        generateRunID();
 
         // Display autorender
         var videoPlayer = document.getElementById("videoPlayer");
