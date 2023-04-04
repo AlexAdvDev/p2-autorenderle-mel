c93ff9a00caab9e8a9e51db2b51928cd714eb686
Jaio <jaio0701@gmail.com>
2023-04-03 20:40:37 -0400

    add ranked guesses

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var mapID = mapsJson[randomMap - 1].mapid;
 console.log("Map ID: " + mapID + " (map number " + randomMap + ")");
 
 var boardJson = null;
+var randomRank = null;
 var runID = null;
 var trueRank = null;
 $(document).ready(function() {
@@ -17,7 +18,7 @@ $(document).ready(function() {
     }).then(function() {
         function generateRunID() {
             // Generate a random rank between 1 and 200 (slightly broken)
-            var randomRank = Math.floor(Math.random() * 200) + 1;
+            randomRank = Math.floor(Math.random() * 200) + 1;
             trueRank = randomRank +1 ;
             console.log("Rank: " + trueRank);
 
@@ -52,10 +53,22 @@ function submitGuess() {
     var guess = submitText.value;
     console.log("Guess: " + guess);
 
-    // Check if guess is correct
+    // Check if guess is correct by margins
     if(guess == trueRank) {
-        alert("Correct!");
+        if(confirm("You guessed exactly right!" + " ran by " + boardJson[Object.keys(boardJson)[randomRank]].userData.boardname)) {
+            window.location.reload();
+        }
+    } else if(trueRank - 10 < guess && guess < trueRank + 10) {
+        if(confirm("You guessed within 10 ranks! The correct answer was " + trueRank + " ran by " + boardJson[Object.keys(boardJson)[randomRank]].userData.boardname)) {
+            window.location.reload();
+        }
+    } else if(trueRank - 20 < guess && guess < trueRank + 20) {
+        if(confirm("You guessed within 20 ranks! The correct answer was " + trueRank + " ran by " + boardJson[Object.keys(boardJson)[randomRank]].userData.boardname)) {
+            window.location.reload();
+        }
     } else {
-        alert("Incorrect!");
+        if(confirm("You guessed wrong! The correct answer was " + trueRank + " ran by " + boardJson[Object.keys(boardJson)[randomRank]].userData.boardname)) {
+            window.location.reload();
+        }
     }
 }
